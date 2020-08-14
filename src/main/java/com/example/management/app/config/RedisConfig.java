77c5f2858ea356fb3ec3ceb8a511d0449c4a2a6e
whz11 <1195687131@qq.com>
2020-08-14 22:11:10 +0800

    1

--- src/main/java/com/example/management/app/config/RedisConfig.java
+++ src/main/java/com/example/management/app/config/RedisConfig.java
@@ -74,45 +74,14 @@ public class RedisConfig extends CachingConfigurerSupport {
             }
         };
     }
-    /**
-     * 重写Redis序列化方式，使用Json方式:
-     * 当我们的数据存储到Redis的时候，我们的键（key）和值（value）都是通过Spring提供的Serializer序列化到数据库的。RedisTemplate默认使用的是JdkSerializationRedisSerializer，StringRedisTemplate默认使用的是StringRedisSerializer。
-     * Spring Data JPA为我们提供了下面的Serializer：
-     * GenericToStringSerializer、Jackson2JsonRedisSerializer、JacksonJsonRedisSerializer、JdkSerializationRedisSerializer、OxmSerializer、StringRedisSerializer。
-     * 在此我们将自己配置RedisTemplate并定义Serializer。
-     *
-     * @param redisConnectionFactory
-     * @return
-     */
 
-     @Bean
-     public RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory redisConnectionFactory) {
-     RedisTemplate<String, Object> redisTemplate = new RedisTemplate<>();
-     redisTemplate.setConnectionFactory(redisConnectionFactory);
-
-     FastJsonRedisSerializer<Object> fastJsonRedisSerializer = new FastJsonRedisSerializer<>(Object.class);
-     // 全局开启AutoType，不建议使用
-     // ParserConfig.getGlobalInstance().setAutoTypeSupport(true);
-     // 建议使用这种方式，小范围指定白名单
-     ParserConfig.getGlobalInstance().addAccept("com.xiaolyuh.");
-
-     // 设置值（value）的序列化采用FastJsonRedisSerializer。
-     redisTemplate.setValueSerializer(fastJsonRedisSerializer);
-     redisTemplate.setHashValueSerializer(fastJsonRedisSerializer);
-     // 设置键（key）的序列化采用StringRedisSerializer。
-     redisTemplate.setKeySerializer(new StringRedisSerializer());
-     redisTemplate.setHashKeySerializer(new StringRedisSerializer());
-
-     redisTemplate.afterPropertiesSet();
-     return redisTemplate;
-     }
 
 
 
 
     /**
      * redis序列化规则配置
-     *
+     */
     @Bean
     @SuppressWarnings("all")
     public RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory factory) {
@@ -139,6 +108,6 @@ public class RedisConfig extends CachingConfigurerSupport {
 
 
 
-*/
+
 
 }
