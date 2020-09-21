f9b4ac24f10af5f621edc8a8c7c41eea5bdcaf82
hhy150 <2334813735@qq.com>
2020-09-22 01:12:59 +0800

    update cors config

--- src/main/java/com/example/management/app/config/CorsConfiguration.java
+++ src/main/java/com/example/management/app/config/CorsConfiguration.java
@@ -16,7 +16,7 @@ public class CorsConfiguration implements WebMvcConfigurer {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**").
-                        allowedOrigins("*"). //允许跨域的域名，可以用*表示允许任何域名使用
+                        allowedOrigins("http://127.0.0.1:5500"). //允许跨域的域名，可以用*表示允许任何域名使用
                         allowedMethods("*"). //允许任何方法（post、get等）
                         allowedHeaders("*"). //允许任何请求头
                         allowCredentials(true). //带上cookie信息
